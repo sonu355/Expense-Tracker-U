f9134675d6810b568e8a550e2053ed4b297ed381
harshal <harshalc562@gmail.com>
2023-04-13 17:05:29 +0530

    input form

---
+++ src/componenets/newExpenses/NewExpenses.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NewExpenses.css'
+import './'
+
+ const NewExpenses = () => {
+  return (
+    <div className='new-expense'>
+      
+    </div>
+  )
+}
+export default NewExpenses;
